25d53952d8f5ec9d510cd13974410a1820490850
Aleksandr Fofanov <avl.fofanov@gmail.com>
2022-06-21 18:08:06 +0300

    make script args more user friendly

--- tools/create-replay.sh
+++ tools/create-replay.sh
@@ -1,18 +1,113 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-BASE_DIR=$(dirname $0)
+set -eof pipefail
+
+function Usage() {
+    cat <<EOM
+##### Create & run traffic replay for an app using ArgoCD
+
+Required arguments:
+    -d | --dest-dir          A path where traffic replay manifest should
+                             be added to
+    -w | --workload-name     A name of the workload to run replay for
+    -s | --snapshot-id       Traffic snapshot identifier
+
+Optional arguments:
+    -t | --test-config-id    Test config id to be used with the replay. Defaults
+                             to standard
+    -b | --build-tag         A build tag to be used with the replay, e.g. CI job
+                             identifier or git commit hash
+    -n | --replay-name       A name of the replay to be created. Defaults to
+                             build tag
+    -h | --help              Show this message and exit
+
+Requirements:
+    speedctl:                Speedscale CLI https://speedscale.com/cli-download/
+
+Example:
+    create-replay.sh \\
+        -d podtato \\
+        -w podtato-head-entry \\
+        -s 41a06065-ec28-438a-b9f4-0e976c6f64ca
+EOM
+    exit 127
+}
+
+function Require {
+    command -v $1 > /dev/null 2>&1 || {
+        echo "Some of the required software is not installed: $1"
+        exit 1;
+    }
+}
+
+# show usage if no args provided
+if [[ $# == 0 ]] ; then Usage; fi
+
+# validate requirements
+Require speedctl
+
+# parse args
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -d|--dest-dir)
+        DEST_DIR="${2}"
+        shift
+        ;;
+    -w|--workload-name)
+        WORKLOAD_NAME="${2}"
+        shift
+        ;;
+    -s|--snapshot-id)
+        SNAPSHOT_ID="${2}"
+        shift
+        ;;
+    -b|--build-tag)
+        BUILD_TAG="${2}"
+        shift
+        ;;
+    -t|--test-config-id)
+        TEST_CONFIG_ID="${2}"
+        shift
+        ;;
+    -n|--name)
+        REPLAY_NAME="${2}"
+        shift
+        ;;
+    -h|--help)
+        Usage
+        ;;
+    *)
+        echo "Unknown argument: ${1}. Pass -h or --help to see usage"
+        exit 3
+        ;;
+  esac
+  shift
+done
+
+BASE_DIR="$(dirname ${BASH_SOURCE[0]})"
 SRC_FILE=${BASE_DIR}/replay-template.yaml
 
-# Required inputs
-DEST_DIR="${1}"
-WORKLOAD_NAME="${2}"
-SNAPSHOT_ID="${3}"
+# validate input
+if [[ -z ${DEST_DIR} ]]; then
+    echo "Destination directory is required"
+    exit 2
+fi
 
-# These inputs have default values
+if [[ -z ${WORKLOAD_NAME} ]]; then
+    echo "Workload name is required"
+    exit 2
+fi
+
+if [[ -z ${SNAPSHOT_ID} ]]; then
+    echo "Snapshot id is required"
+    exit 2
+fi
+
+# set defaults
 EPOCH=$(date +%s)
-BUILD_TAG="${4:-$EPOCH}"
-TEST_CONFIG_ID="${5:-standard}"
-REPLAY_NAME="${6:-$BUILD_TAG}"
+BUILD_TAG=${BUILD_TAG:-$EPOCH}
+TEST_CONFIG_ID="${TEST_CONFIG_ID:-standard}"
+REPLAY_NAME="${REPLAY_NAME:-$BUILD_TAG}"
 
 # Start doing substitutions
 DEST_FILE=${DEST_DIR}/replay-${REPLAY_NAME}.yaml
