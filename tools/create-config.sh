4c9bb7db8270fd3967a1ecfbe0962c057c3d0101
Ken Ahrens <ken.ahrens@gmail.com>
2022-06-21 20:07:50 -0400

    update config

--- tools/create-config.sh
+++ tools/create-config.sh
@@ -5,6 +5,7 @@ BASE_DIR="$(dirname ${BASH_SOURCE[0]})"
 mkdir ~/.speedscale
 
 cat ${BASE_DIR}/config-template.yaml | sed \
+  -e "s/APP_URL/${APP_URL}/" \
   -e "s/TENANT_ID/${TENANT_ID}/" \
   -e "s/TENANT_NAME/${TENANT_NAME}/" \
   -e "s/TENANT_BUCKET/${TENANT_BUCKET}/" \
