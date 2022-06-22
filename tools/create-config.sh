b8797e3006798448d944b798c7f69d799d432666
Ken Ahrens <ken.ahrens@gmail.com>
2022-06-21 22:49:19 -0400

    fix config installer

--- tools/create-config.sh
+++ tools/create-config.sh
@@ -10,5 +10,5 @@ cat ${BASE_DIR}/config-template.yaml | sed \
   -e "s/TENANT_NAME/${TENANT_NAME}/" \
   -e "s/TENANT_BUCKET/${TENANT_BUCKET}/" \
   -e "s/TENANT_API_KEY/${TENANT_API_KEY}/" \
-  -e "s/TENANT_STREAM/${TENANT_STREAM}/"
+  -e "s/TENANT_STREAM/${TENANT_STREAM}/" \
   > ~/.speedscale/config.yaml
