22924f89a1f0e4f5566961d8ca1cd5944e9c8f04
Ken Ahrens <ken.ahrens@gmail.com>
2022-06-21 22:43:09 -0400

    fix create config script

--- tools/create-config.sh
+++ tools/create-config.sh
@@ -12,5 +12,3 @@ cat ${BASE_DIR}/config-template.yaml | sed \
   -e "s/TENANT_API_KEY/${TENANT_API_KEY}/" \
   -e "s/TENANT_STREAM/${TENANT_STREAM}/"
   > ~/.speedscale/config.yaml
-
-sh -c "$(curl -sL https://downloads.speedscale.com/speedctl/install)"
